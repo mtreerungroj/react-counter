9673bbf81819fb2d8d10d47bf00dd219a6fbc97e
mtreerungroj <m.treerungroj@gmail.com>
2018-03-17 10:29:49 +0700

    create store

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { createStore } from 'redux'
+
+let reducer = (state = { val: 0 }, action) => {
+  if (action === 'INCREMENT') {
+    return { val: state.val + action.by }
+  }
+  return state
+}
+let store = createStore(reducer)
 
 ReactDOM.render(<App />, document.getElementById('root'))
