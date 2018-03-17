bc2cdd31a45116ab48bd36a1dd17c7adcb2e6f36
mtreerungroj <m.treerungroj@gmail.com>
2018-03-17 11:50:49 +0700

    add middleware

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 
 let reducer = (state = { val: 0 }, action) => {
@@ -14,7 +14,14 @@ let reducer = (state = { val: 0 }, action) => {
   return state
 }
 
-let store = createStore(reducer)
+// store อันไหน, next ให้ไปต่อหรือไม่, action ที่จะเรียกคืออะไร
+let middleware = store => next => action => {
+  if (store.getState().val < 5) {
+    next(action)
+  }
+}
+
+let store = createStore(reducer, applyMiddleware(middleware))
 // setInterval(() => store.dispatch({ type: 'INCREMENT', by: 1 }), 1000)
 
 ReactDOM.render(
