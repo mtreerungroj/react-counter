1ca727f6971bb457905d67d46940450f43d1c09d
mtreerungroj <m.treerungroj@gmail.com>
2018-03-17 13:26:34 +0700

    add alternative for incWithDelay

--- src/middlewares/actionDelay.js
+++ src/middlewares/actionDelay.js
@@ -1,8 +1,10 @@
 let middleware = store => next => action => {
   if (action.__delay && action.__delay > 0) {
     setTimeout(() => {
-      delete action.__delay //  ถ้าไม่ delete ออกมันจะ delay ไปเรื่อยๆ ไม่สิ้นสุด
-      store.dispatch(action)
+      // delete action.__delay //  ถ้าไม่ delete ออกมันจะ delay ไปเรื่อยๆ ไม่สิ้นสุด
+      // store.dispatch(action) // สั่ง dispatch มันจะเข้า Middleware อีกทีนึง
+      // alternative
+      next(action)
     }, action.__delay)
     return
   }
