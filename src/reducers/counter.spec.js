94925139c13a14b9c721d853522d5cfebde56d4b
mtreerungroj <m.treerungroj@gmail.com>
2018-03-17 13:40:58 +0700

    add more test case

--- src/reducers/counter.spec.js
+++ src/reducers/counter.spec.js
@@ -1,7 +1,16 @@
 import reducer from './counter'
+import { inc, reset } from '../actions'
 
 describe('counter reducer', () => {
   it('should handle initial state', () => {
     expect(reducer()).toEqual({ val: 0 })
+  })
+
+  it('should increment', () => {
+    expect(reducer({ val: 1 }, inc())).toEqual({ val: 2 })
+  })
+
+  it('should reset', () => {
+    expect(reducer({ val: 7 }, reset())).toEqual({ val: 0 })
   })
 })
